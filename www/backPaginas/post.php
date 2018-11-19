fdabeae93ec0c3e4860c041ef9ee5fb656c45849
Guilherme Veras <guilherme.2000@alunos.utfpr.edu.br>
2018-11-19 15:16:01 -0200

    Adicao do contador de visualizacoes

--- www/backPaginas/post.php
+++ www/backPaginas/post.php
@@ -14,5 +14,6 @@ join metodologias on pos_metodologia=met_id
 where pos_id=$post;");
 $dados = $result->fetch(PDO::FETCH_ASSOC);
 
+$conn->query("UPDATE posts SET pos_visualizacoes=pos_visualizacoes+1 where pos_id=$post");
 
 ?>
