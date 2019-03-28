f7ee395ba2f4c8bc93cd3f47a14b96d071d66d9c
Guilherme <guilherme.2000@alunos.utfpr.edu.br>
2019-03-28 16:03:04 -0300

    Correcao

--- www/backPaginas/contato.php
+++ www/backPaginas/contato.php
@@ -5,12 +5,12 @@ if (isset($_POST['email'])) {
 	$text = $email.": ".$_POST['text'];
 
 	if ($mailSent = smtpmailer($mailRemei, $mailRemei, 'REMEI', 'Contato', $text)) {
-		echo "<script>alert('Menssagem enviada com sucesso')</script>"
+		echo "<script>alert('Menssagem enviada com sucesso')</script>";
 
 		//Header("location:http://www.dominio.com.br/obrigado.html"); // Redireciona para uma página de obrigado.
 		//print("deu bom");
 	}else{
-		echo "<script>alert('Erro ao enviar a menssagem, tente novamente mais tarde')</script>"
+		echo "<script>alert('Erro ao enviar a menssagem, tente novamente mais tarde')</script>";
 		//print($mailSent);
 	}
 }
--- www/backPaginas/esqueceu_senha.php
+++ www/backPaginas/esqueceu_senha.php
@@ -12,16 +12,16 @@ if (isset($_POST['email'])) {
 		$text = "Abre esse link para gerar uma nova senha: www.remei.sh.utfpr.edu.br/nova_senha?i=".$dado;
 
 		if ($mailSent = smtpmailer($email, $mailRemei, 'REMEI', 'Esqueci minha senha', $text)) {
-			echo "<script>alert('Abre seu email para gerar uma nova senha')</script>"
+			echo "<script>alert('Abre seu email para gerar uma nova senha')</script>";
 
 			//Header("location:http://www.dominio.com.br/obrigado.html"); // Redireciona para uma página de obrigado.
 			//print("deu bom");
 		}else{
-			echo "<script>alert('Erro ao enviar a menssagem, tente novamente mais tarde')</script>"
+			echo "<script>alert('Erro ao enviar a menssagem, tente novamente mais tarde')</script>";
 			//print($mailSent);
 		}
 	}else{
-		echo "<script>alert('Email incorreto, tente novamente')</script>"
+		echo "<script>alert('Email incorreto, tente novamente')</script>";
 	}
 
 
