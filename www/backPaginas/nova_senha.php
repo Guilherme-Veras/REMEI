6d609deaad82ee98456aa8495ef5bfcae5de4c0a
Guilherme <guilherme.2000@alunos.utfpr.edu.br>
2019-03-28 18:01:11 -0300

    Server

--- www/backPaginas/nova_senha.php
+++ www/backPaginas/nova_senha.php
@@ -7,7 +7,7 @@ if (isset($_GET['i'])) {
 }
 
 if (isset($_POST['senha'])) {
-	$result = $conn->query("UPDATE users set usu_senha='".sha1($_POST['senha'])."' where usu_senha = 'id'");
+	$result = $conn->query("UPDATE users set usu_senha='".sha1($_POST['senha'])."' where usu_senha = '$id'");
 	print("UPDATE users set usu_senha='".sha1($_POST['senha'])."' where usu_senha='id'");
 }
 
