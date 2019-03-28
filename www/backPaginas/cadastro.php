38db2d1186a55e4a050d033d91dbdbdb08cdf70f
Guilherme <guilherme.2000@alunos.utfpr.edu.br>
2019-03-28 18:28:14 -0300

    Server

--- www/backPaginas/cadastro.php
+++ www/backPaginas/cadastro.php
@@ -11,22 +11,16 @@ if (isset($_POST['email'])) {
 	if($result->rowCount() > 0){
 		echo 0;
 	}else{
-		error_reporting(E_ALL);
-		ini_set('display_errors', 1);
 		$result = $conn->query("INSERT INTO users(usu_nome,usu_email,usu_senha) values('$nome','$email','$senha');");
 		if ($result) {
 			$id = $conn->query("SELECT usu_id from users where usu_email='$email'")->fetch(PDO::FETCH_ASSOC)['usu_id'];
 			$_SESSION['user'] = $id;
 			$_SESSION['userNome'] = $nome;
-			//echo getcwd();
-			chdir("imgs/perfil");
-			var_dump(scandir(getcwd()));
-			if (!copy("0.jpg", $id.".jpg")) {
+			if (!copy("imgs/perfil/0.jpg", "imgs/perfil/".$id.".jpg")) {
 			    echo -2;
 			}else{
 				echo 1;
 			}
-			chdir("../..");
 			//inicio();
 		}else{
 			echo -1;
