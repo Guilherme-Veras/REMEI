b3f45494b7058510e08ddcc3234ea215574a104f
Guilherme <guilherme.2000@alunos.utfpr.edu.br>
2019-03-28 18:47:32 -0300

    Server

--- www/apis/areas.php
+++ www/apis/areas.php
@@ -5,7 +5,7 @@ $texto = $_POST['entrada'];
 $result = $conn->query("SELECT are_id, are_nome from areas where are_nome like '%$texto%'");
 $res = array();
 for ($i=0; $i < $result->rowCount(); $i++) { 
-	array_push($res, $result->fetch(PDO::FETCH_ASSOC));
+	array_push($res, array_map("utf8_encode", $result->fetch(PDO::FETCH_ASSOC)));
 }
 echo json_encode($res);
 ?>
--- www/apis/metodologias.php
+++ www/apis/metodologias.php
@@ -1,15 +1,11 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$texto = $_GET['entrada'];
-print($texto);
+
+$texto = $_POST['entrada'];
+
 $result = $conn->query("SELECT met_id, met_nome from metodologias where met_nome like '%$texto%'");
 $res = array();
 for ($i=0; $i < $result->rowCount(); $i++) { 
 	array_push($res, array_map("utf8_encode", $result->fetch(PDO::FETCH_ASSOC)));
 }
-var_dump($res);
-echo "---";
 echo json_encode($res);
-echo "---";
 ?>
