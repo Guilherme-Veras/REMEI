896b57827047c75cc0dc3f9618296518ba7da6a1
Guilherme <guilherme.2000@alunos.utfpr.edu.br>
2019-03-28 18:45:51 -0300

    Server

--- www/apis/metodologias.php
+++ www/apis/metodologias.php
@@ -6,10 +6,10 @@ print($texto);
 $result = $conn->query("SELECT met_id, met_nome from metodologias where met_nome like '%$texto%'");
 $res = array();
 for ($i=0; $i < $result->rowCount(); $i++) { 
-	array_push($res, $result->fetch(PDO::FETCH_ASSOC));
+	array_push($res, utf8_encode($result->fetch(PDO::FETCH_ASSOC)));
 }
 var_dump($res);
 echo "---";
-echo json_encode(utf8ize($res));
+echo json_encode($res);
 echo "---";
 ?>
